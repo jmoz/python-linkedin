6ad2462e037502f63397e0851d4c6c855c480cbb
James Morris <james@jmoz.co.uk>
2013-02-25 13:38:59 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="python-linkedin",
+    description="python-linkedin",
+    url="http://github.com/jmoz/python-linkedin",
+    packages = find_packages(),
+    keywords= "linkedin api python")
